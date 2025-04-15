08d957ca5cfcdf6772bb41dda9b72ee5044e3305
Glen Takahashi <glen@glentaka.com>
2025-04-14 18:51:17 -0700

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/pack-simulator/',
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'],
   resolve: {
     alias: {
